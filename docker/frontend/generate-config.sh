4f4f0889701f916d494a5ba99fe4258a250553e1
Tomas Carbone <tomascarbone214@gmail.com>
2026-07-03 21:15:48 -0300

    .BAT  Y .SH para ejecuion configuracion mediante .env sin docker

--- docker/frontend/generate-config.sh
+++ docker/frontend/generate-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-envsubst '${ESP32_IP} ${BACKEND_IP} ${BACKEND_PORT} ${BACKEND_CONTEXT_PATH}' \
+envsubst '${ESP32_IP} ${MQTT_IP} ${BACKEND_PORT} ${BACKEND_CONTEXT_PATH}' \
   < /usr/share/nginx/html/js/config.js.template \
   > /usr/share/nginx/html/js/config.js
